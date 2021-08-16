121b71ec9cc9051a7024d0fd37739b2b24825062
danilo cucharro <danicucharro@outlook.com>
2021-08-16 19:25:37 -0300

    configurando webhooks do stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -65,8 +65,5 @@ export default async (req: NextApiRequest, res:NextApiResponse)=>{
 
         return res.status(200).json({sessionId: stripeCheckoutSession.id})
     }
-    else{
-        res.setHeader('Allow', 'POST')
-        res.status(405).end('Method not allowed')
-    }
+    
 }
